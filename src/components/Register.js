f10ea64f8386fc5fdf86845c3634405b020aa3cc
Geetanjali <geetakorgaonkar93@gmail.com>
2020-03-20 18:29:07 +0530

    Fields added for register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
         };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
     }
 
     render() {
         return (
             <div className="register-form">
                 <h1>Register user</h1>
+                <form onSubmit={this.handleSubmit} className="form-wrap">
+                    <div className="input-box">
+                        <label htmlFor="name">Name: </label>
+                        <input type="text" id="name" />
+                    </div>
+                    <div className="input-box">
+                        <label htmlFor="contactNumber">Contact Number: </label>
+                        <input type="text" id="contactNumber" />
+                    </div>
+                    <div className="input-box">
+                        <label htmlFor="email">Email: </label>
+                        <input type="text" id="email" />
+                    </div>
+                    <div className="input-box">
+                        <label htmlFor="username">Username: </label>
+                        <input type="text" id="username" />
+                    </div>
+                    <div className="input-box">
+                        <label htmlFor="password">Password: </label>
+                        <input type="text" id="password" />
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
+                <Link to='/'>Cancel</Link>
             </div>
         )
     }
